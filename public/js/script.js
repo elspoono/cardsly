217d289ec0e31657c2112a5090c2785578b2bb63
Derek Bredensteiner <elspoono@gmail.com>
2011-10-21 16:17:06 -0700

    Styling new

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
 (function() {
-  $(function() {});
+  $(function() {
+    return $('.add-button').hover(function() {
+      return $(this).addClass('hover');
+    }, function() {
+      return $(this).removeClass('hover');
+    }).mousedown(function() {
+      return $(this).addClass('click');
+    }).mouseup(function() {
+      return $(this).removeClass('click');
+    });
+  });
 }).call(this);
--- src/public/js/script.coffee
+++ src/public/js/script.coffee
@@ -1,2 +1,10 @@
 $ ->
   # function goes here
+  $('.add-button').hover ->
+    $(this).addClass 'hover'
+  ,->
+    $(this).removeClass 'hover'
+  .mousedown ->
+    $(this).addClass 'click'
+  .mouseup ->
+    $(this).removeClass 'click'
