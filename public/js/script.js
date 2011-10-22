92072e64dc903f7744fa5e338a7893be7d005915
Derek Bredensteiner <elspoono@gmail.com>
2011-10-21 22:42:35 -0700

    Styling new

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 (function() {
   $(function() {
-    return $('.add-button,.checkout-button').hover(function() {
+    var advanceSlide, marginIncrement, maxSlides, newMargin, timer;
+    $('.button').hover(function() {
       return $(this).addClass('hover');
     }, function() {
       return $(this).removeClass('hover');
@@ -9,5 +10,39 @@
     }).mouseup(function() {
       return $(this).removeClass('click');
     });
+    newMargin = 0;
+    maxSlides = 2;
+    marginIncrement = 620;
+    maxSlides--;
+    advanceSlide = function() {
+      if (newMargin < maxSlides * -marginIncrement) {
+        newMargin = 0;
+      } else if (newMargin > 0) {
+        newMargin = maxSlides * -marginIncrement;
+      }
+      return $('.slides .content').animate({
+        'margin-left': newMargin
+      }, 400);
+    };
+    $('.slides .arrow-right').click(function() {
+      var timer;
+      timer = 0;
+      newMargin -= marginIncrement;
+      return advanceSlide();
+    });
+    $('.slides .arrow-left').click(function() {
+      var timer;
+      timer = 0;
+      newMargin -= -marginIncrement;
+      return advanceSlide();
+    });
+    return timer = setTimeout(function() {
+      newMargin -= marginIncrement;
+      advanceSlide();
+      return timer = setInterval(function() {
+        newMargin -= marginIncrement;
+        return advanceSlide();
+      }, 5000);
+    }, 2000);
   });
 }).call(this);
--- src/public/js/script.coffee
+++ src/public/js/script.coffee
@@ -1,6 +1,6 @@
 $ ->
   # function goes here
-  $('.add-button,.checkout-button').hover ->
+  $('.button').hover ->
     $(this).addClass 'hover'
   ,->
     $(this).removeClass 'hover'
@@ -8,3 +8,37 @@ $ ->
     $(this).addClass 'click'
   .mouseup ->
     $(this).removeClass 'click'
+
+  # Define Margin
+  newMargin = 0
+  maxSlides = 2
+  marginIncrement = 620
+  maxSlides--
+
+  advanceSlide = ->
+    if newMargin < maxSlides * -marginIncrement
+      newMargin=0
+    else if newMargin > 0
+      newMargin = maxSlides * -marginIncrement
+
+    $('.slides .content').animate
+      'margin-left': newMargin
+    , 400
+
+  $('.slides .arrow-right').click ->
+    timer = 0
+    newMargin -= marginIncrement
+    advanceSlide()
+  $('.slides .arrow-left').click ->
+    timer = 0
+    newMargin -= -marginIncrement
+    advanceSlide()
+
+  timer = setTimeout ->
+    newMargin -= marginIncrement
+    advanceSlide()
+    timer = setInterval ->
+      newMargin -= marginIncrement
+      advanceSlide()
+    , 5000
+  , 2000
