7146087550668f954ca88c9a1dd4c45605a1a082
Derek Bredensteiner <elspoono@gmail.com>
2011-11-22 12:04:15 -0700

    make redirect to https on production only

--- app.js
+++ app.js
@@ -1100,17 +1100,19 @@
     return !err;
   };
 
-  app.get('*', function(req, res, next) {
-    var headers;
-    headers = req.headers;
-    if (headers['x-real-ip'] && headers['x-forwarded-proto'] !== 'https') {
-      return res.send('', {
-        Location: 'https://www.cards.ly' + req.url
-      }, 302);
-    } else {
-      return next();
-    }
-  });
+  if (app.settings.env === 'production') {
+    app.get('*', function(req, res, next) {
+      var headers;
+      headers = req.headers;
+      if (headers['x-real-ip'] && headers['x-forwarded-proto'] !== 'https') {
+        return res.send('', {
+          Location: 'https://www.cards.ly' + req.url
+        }, 302);
+      } else {
+        return next();
+      }
+    });
+  }
 
   app.get('/', function(req, res) {
     return res.render('landing-prelaunch', {
